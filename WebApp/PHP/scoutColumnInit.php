458cd4d4d1288bf132b4411c185826b69f6eb792
Yasha Mostofi <yasha.mostofi@gmail.com>
2014-01-17 11:02:24 -0600

    More madness

--- WebApp/PHP/scoutColumnInit.php
+++ WebApp/PHP/scoutColumnInit.php
@@ -1,20 +1,4 @@
-
 <?php
-
-echo <<<_END
-<html>
-<head>
-    <title>Creating columns...</title>
-</head>
-<body>
-<form method="post" action="scoutCreateDatabase.php">
-           name of database?: 
-        <input type="text" name="databaseName" />
-        <input type="submit" />
-</form>
-</body>
-</html>
-_END;
 include 'GLOBAL.php';
 if ($usePassword) {
     $conn = mysql_connect("localhost", $databaseUser, $databasePassword);
