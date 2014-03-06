e5d1f1d8c6e88d4189aaf7ef86b7d884a6cc05ed
nnrobot <scouting@yashamostofi.com>
2014-03-05 21:54:19 -0600

    updated test for scouting

--- WEB/util/test.php
+++ WEB/util/test.php
@@ -2,5 +2,15 @@
 
 include 'GLOBAL.php';
 
+echo count($teamFieldNames);
+echo count($teamFieldNameTypes);
+
+echo (":");
+
+echo count($allianceFieldNames);
+echo count($allianceFieldNameTypes);
+
+echo (":");
+
 echo count($pitFieldNames);
 echo count($pitFieldNameTypes);
